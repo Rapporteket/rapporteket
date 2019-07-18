7017865986e60cb36a1d4f28f91bcde28973837a
Are Edvardsen <are.edvardsen@helse-nord.no>
2019-07-18 12:33:03 +0200

    package meta function

---
+++ R/rapporteket.R
@@ -0,0 +1,9 @@
+#' rapporteket: A package containing information regarding Rapporteket
+#'
+#' The rapporteket package provides information regarding Rapporteket: what it
+#' is, how to get stated, regular use and development of content. As such, this
+#' packages cointaines no (or very few) R functions
+#'
+#' @docType package
+#' @name rapporteket
+NULL
